5b53bdda8b3792e1a7d45cecef57eb775f027235
filips <filips@filipss-MacBook-Pro.local>
2024-07-09 11:52:10 +0200

    Fix for lazy

--- lua/fikolis/init.lua
+++ lua/fikolis/init.lua
@@ -5,9 +5,9 @@ require ("fikolis.lazy_init")
 -- Set file type for YAML files with .yaml and .yml extensions
 vim.api.nvim_exec([[
     augroup FileTypeYAML
-        autocmd!
-        autocmd BufNewFile,BufRead */ansible*/*.yaml,*/ansible*/*.yml, */*ansible*/*.yaml, */*ansible*/*.yml setl ft=yaml.ansible
-    augroup END
+    autocmd!
+    autocmd BufNewFile,BufRead */ansible*/*.yaml,*/ansible*/*.yml,*/*ansible*/*.yaml,*/*ansible*/*.yml setl ft=yaml.ansible
+augroup END
 ]], false)
 
 local augroup = vim.api.nvim_create_augroup
--- lua/fikolis/lazy/lsp.lua
+++ lua/fikolis/lazy/lsp.lua
@@ -28,6 +28,7 @@ return {
             ensure_installed = {
                 "lua_ls",
                 "rust_analyzer",
+                "ansiblels",
                 "tsserver",
             },
             handlers = {
